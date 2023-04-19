38dc650d2ddf5b1851c84ac6d75dea0ba8645f5d
himaja-e <105623310+himaja-e@users.noreply.github.com>
2023-04-19 23:02:17 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -10,6 +10,13 @@ b = st.number_input("Enter the second number")
 c = st.number_input("Enter the third number")
 
 if st.button("Find the largest"):
-    result = largest_num(num1, num2, num3)
-    st.success(f"The largest number is {result}")
+    if(a>b and a>c):
+        l=a
+    elif(b>a and b>c):
+        l=b
+    elif(c>a and c>b):
+        l=c
+    else:
+        l=a
+    st.success(f"The largest number is {l}")
 
