85163cfd31cb8e00b6c1a23088db848f3089cdcc
himaja-e <105623310+himaja-e@users.noreply.github.com>
2023-04-19 22:56:08 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,20 +1,15 @@
 import streamlit as st
 
-st.title("Largest of three numbers")
-a=st.number_input(int(input("Enter the first number")))
-b=st.number_input(int(input("Enter the second number")))
-c=st.number_input(int(input("Enter the third number")))
+def largest_num(a, b, c):
+    return max(a, b, c)
 
-if st.button("Find largest"):
-  l=a
-  if(a>b and a>c):
-    l=a
-  elif(b>a and b>c):
-    l=b
-  elif(c>a and c>b):
-    l=c
-  else:
-    l=a
+st.title("Find the largest of three numbers")
 
-  st.write("Largest number is", l)
+num1 = st.number_input("Enter the first number")
+num2 = st.number_input("Enter the second number")
+num3 = st.number_input("Enter the third number")
+
+if st.button("Find the largest"):
+    result = largest_num(num1, num2, num3)
+    st.success(f"The largest number is {result}")
 
